ba29aa03161b0bb38f2305032a3e5cdb61508d94
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2015-06-10 17:20:09 -0400

    adding the javascript variable from the python

--- ckanext/extlink/public/extlink.js
+++ ckanext/extlink/public/extlink.js
@@ -1,34 +1,6 @@
 jQuery(function($){
 var href;
-var urls = [
-"performance.gov",
-"usa.gov",
-"whitehouse.gov",
-"www.data.gov",
-"data.gov",
-"catalog.data.gov",
-"ars-grin.gov",
-"mrlc.gov",
-"epa.gov",
-"whitehouse.gov",
-"bison.usgs.ornl.gov",
-"2014.spaceappschallenge.org",
-"stateofthecoast.noaa.gov",
-"csc.noaa.gov",
-"www.csc.noaa.gov",
-"edg.epa.gov",
-"www.marinecadastre.gov",
-"marineprotectedareas.noaa.gov",
-"geoplatform.gov",
-"catalog.data.gov",
-"ridb.recreation.gov",
-"dev-datagov.reisys.com",
-"qa-datagov.reisys.com",
-"uat-datagov.reisys.com",
-".gov/",
-".mil",
-".gov"
-];
+var urls = extlink_white_list;
 
 var $a = $('a');
    $a.each((function(i){
@@ -46,7 +18,7 @@ var $a = $('a');
     }(i)));
 $("a:not([href^='http://'])").addClass("non_ext");
 });
-$( document ).ready(function() {
+jQuery( document ).ready(function() {
 jQuery('a').each(function(){
 
 
@@ -57,7 +29,7 @@ jQuery('a').each(function(){
             jQuery(this).attr('title', 'This link will direct you to an external website that may have different content and privacy policies from catalog.Data.gov.')
                 }
 }
+});
 
 
-
-        });
+ });
