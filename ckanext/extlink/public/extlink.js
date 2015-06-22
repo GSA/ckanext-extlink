f40d343d2d20bfa1e524eb2c1053bd2444ec59d5
kapil.bulchandani <kapil.bulchandani@reisystems.com>
2015-06-22 17:28:54 -0400

    adding the change to master

--- ckanext/extlink/public/extlink.js
+++ ckanext/extlink/public/extlink.js
@@ -1,48 +1,59 @@
 jQuery(function($){
-var href;
-var urls = extlink_white_list;
-var i;
-var $a = $('a');
-   $a.each(function(i){
+    var href;
+    var urls = extlink_white_list;
+    var i;
+    var $a = $('a');
+    $a.each(function(i){
         return function(){
-          for (href = 0; href < urls.length; href++) {
-              if (this.href.indexOf(urls[href]) != -1) {
+            for (href = 0; href < urls.length; href++) {
+                if (this.href.indexOf(urls[href]) != -1) {
 
-             $( this ).addClass( "non_ext" );
+                    $( this ).addClass( "non_ext" );
 
-             }
+                }
 
-          }
+            }
 
         }
     }(i));
-$("a:not([href^='http://'])").addClass("non_ext");
+    $("a:not([href^='http://'])").addClass("non_ext");
 });
 jQuery( document ).ready(function() {
-jQuery('a').each(function(){
-var ext_msg= extlink_popup_message;
-    if(ext_msg == "" || ext_msg == " " || ext_msg.length == 0 ){
+    jQuery('a').each(function(){
+        var ext_msg= extlink_popup_message;
+        //if(ext_msg == "" || ext_msg == " " || ext_msg.length == 0 ){
 
-        ext_msg='This link will direct you to an external website that may have different content and privacy policies from Data.gov.'
-    }
-if(jQuery(this).hasClass("ext_link")){
-  if (!jQuery(this).attr('title')) {
-            jQuery(this).attr('title', ext_msg);
-                }
-}
-            if(!jQuery(this).hasClass("non_ext")){
+        // ext_msg='This link will direct you to an external website that may have different content and privacy policies from Data.gov.'
+        //}
+        if(jQuery(this).hasClass("ext_link")){
+            if (!jQuery(this).attr('title')) {
+                jQuery(this).attr('title', ext_msg);
+            }
+        }
+
+        if(!jQuery(this).hasClass("non_ext")){
 
             jQuery(this).addClass("ext_link");
             if (!jQuery(this).attr('title')) {
-            jQuery(this).attr('title', ext_msg);
-                }
-}
+                jQuery(this).attr('title', ext_msg);
+            }
+        }
+    });
+
+    jQuery( ".ext_link" ).mouseover(function() {
+
+        jQuery( ".dataset-item").css( "overflow","visible" );
+
+    })
+
+        .mouseout(function() {
+
+            jQuery( ".dataset-item").css( "overflow","hidden" );
+
+        });
 });
+jQuery(function() {
 
-
- });
-$(function() {
-
-    $( '.ext_link' ).tooltip();
+    jQuery( '.ext_link' ).tooltip();
 
 });
