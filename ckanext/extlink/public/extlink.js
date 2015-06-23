42c327689b358d7ae0cd4d5a37947d687999f9a6
fxia <fxia@reisystems.com>
2015-06-23 12:10:58 -0400

    simplify and fix js codes

--- ckanext/extlink/public/extlink.js
+++ ckanext/extlink/public/extlink.js
@@ -1,39 +1,41 @@
 /* function to check white list domain */
 
 jQuery(function($){
-    var href;
     var urls = extlink_white_list;
-    var i;
-    var $a = $('a');
-    $a.each(function(i){
-        return function(){
-            for (href = 0; href < urls.length; href++) {
-
-                var domains = this.href.split( '/' );
-                var d = domains[2];
-
-                if (d == null || d == ""){
-
-                    $( this ).addClass( "non_ext" );
-                }
-                else if(d.indexOf(urls[href]) != -1) {
-                    // console.log(("twitter.com").indexOf(urls[href]));
-
-                    $( this ).addClass( "non_ext" );
-                }
+    // add self domain to the array
+    urls.push(window.location.host)
+    $('a').each(function(){
+        if (!this.host) { // exit if it is a fake a without href
+            $( this ).addClass( "non_ext" );
+            return;
+        };
+        for (i = 0; i < urls.length; i++) {
+            if (!urls[i]) continue;
+            parts = this.host.split(urls[i])
+            // add non_ext...
+            if (
+                // ...if endswith white listed url
+                this.host.indexOf(urls[i],this.host.length - urls[i].length) !== -1
+                &&
+                (
+                // ...white listed cd.com should not cover www.abcd.com
+                    urls[i].indexOf('.') === 0
+                    ||
+                    urls[i] == this.host
+                    ||
+                    parts[parts.length - 2].slice(-1) == '.'
+                )
+            ) {
+                $( this ).addClass( "non_ext" );
+                break;
             }
         }
-    }(i));
-    $("a:not([href^='http://']):not([href^='https://'])").addClass("non_ext");
-
+    });
 });
+
 jQuery( document ).ready(function() {
     jQuery('a').each(function(){
         var ext_msg= extlink_popup_message;
-        //if(ext_msg == "" || ext_msg == " " || ext_msg.length == 0 ){
-
-        // ext_msg='This link will direct you to an external website that may have different content and privacy policies from Data.gov.'
-        //}
         if(jQuery(this).hasClass("ext_link")){
             if (!jQuery(this).attr('title')) {
                 jQuery(this).attr('title', ext_msg);
@@ -48,19 +50,8 @@ jQuery( document ).ready(function() {
             }
         }
     });
-
-    jQuery( ".ext_link" ).mouseover(function() {
-
-        jQuery( ".dataset-item").css( "overflow","visible" );
-
-    })
-
-        .mouseout(function() {
-
-            jQuery( ".dataset-item").css( "overflow","hidden" );
-
-        });
 });
+
 jQuery(function() {
 
     jQuery( '.ext_link' ).tooltip();
